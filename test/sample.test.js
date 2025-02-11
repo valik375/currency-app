5249dfbb61b18a5cee2502064ee6aba209ef6a2b
valik375 <goloollg@gmail.con>
2025-02-11 18:15:38 +0100

    update code

--- test/sample.test.js
+++ test/sample.test.js
@@ -10,3 +10,32 @@ describe("Sample Mocha Test (ESM)", function () {
     expect(sum).to.equal(5)
   })
 })
+
+function fetchData() {
+  return setTimeout(() => {
+    return { data: 'Hello, world!', age: 23 }
+  }, 1000)
+}
+describe('Asynchronous Testing with done', function () {
+  it('should fetch data correctly', async function () {
+    const result = await fetchData()
+    expect(result).to.have.property('data').that.equals('Hello, Promise!')
+    expect(result).to.have.property('age').that.equals(23)
+  })
+})
+
+function fetchPromiseData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ data: 'Hello, Promise!', age: '32' })
+    }, 1000)
+  })
+}
+describe('Promise-based Testing', function () {
+  it('should fetch data correctly with Promise', function () {
+    return fetchPromiseData().then((result) => {
+      expect(result).to.have.property('data').that.equals('Hello, Promise!')
+      expect(result).to.have.property('age').that.equals(32)
+    })
+  })
+})
